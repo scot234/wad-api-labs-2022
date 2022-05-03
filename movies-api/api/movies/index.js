f831c8cd514610ff1fe33c25517083547fccf4d1
unknown <gbenoba7@gmail.com>
2022-05-03 08:36:18 -0700

    added routing for movies and movie reviews

--- movies-api/api/movies/index.js
+++ movies-api/api/movies/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import { movies, movieReviews, movieDetails } from './moviesData';
+import uniqid from 'uniqid'
 
 const router = express.Router(); 
 router.get('/:id', (req, res) => {
@@ -13,5 +14,23 @@ router.get('/:id', (req, res) => {
         });
     }
 });
+
+router.post('/:id/reviews', (req, res) => {
+    const id = parseInt(req.params.id);
+
+    if (movieReviews.id == id) {
+        req.body.created_at = new Date();
+        req.body.updated_at = new Date();
+        req.body.id = uniqid();
+        movieReviews.results.push(req.body); //push the new review onto the list
+        res.status(201).json(req.body);
+    } else {
+        res.status(404).json({
+            message: 'The resource you requested could not be found.',
+            status_code: 404
+        });
+    }
+});
+
 
 export default router;
